de6283060e1f9c606a3882ef7930d5a35cf5a5d5
Aaditya Taparia <aadityataparia@gmail.com>
2018-02-14 16:40:44 +0900

    fetch to remove not something we can emrge error

--- onetime.sh
+++ onetime.sh
@@ -1,5 +1,8 @@
 git init
 git remote add origin $1
+git fetch origin
+git checkout origin/master
+git checkout -b master
 git merge origin/master
 git branch --set-upstream-to origin/master master
 git remote update -p; git merge --ff-only @{u}
