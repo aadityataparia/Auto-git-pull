588bcbfc2ddca38aeb616fce65a6959fd7cc1af4
Aaditya Taparia <aadityataparia@gmail.com>
2018-03-06 19:42:02 +0900

    add quotess

--- git.php
+++ git.php
@@ -9,7 +9,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     $branch = (isset($_GET['branch'] ? $_GET['branch'] : 'master')
-    echo shell_exec("./git.sh {$branch} 2>&1");
+    echo shell_exec("./git.sh \"{$branch}\" 2>&1");
     ?>
   </body>
 </html>
