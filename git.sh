54b8a030acb5ffef5c70a3f20c039fac21056901
Aaditya Taparia <aadityataparia@gmail.com>
2018-03-12 22:59:26 +0900

    reset local changes

--- git.sh
+++ git.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 git checkout -b $1 || git checkout $1
-git branch --set-upstream-to origin/$1
-git stash save --keep-index
-git remote update -p; git merge --ff-only @{u}
+git fetch --all
+git reset --hard origin/$1
